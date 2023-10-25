513c95243995b1770d81a9361be57f6051e08198
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-10-25 11:36:01 +0700

    Update Week 6

---
+++ lib/booking.dart
@@ -0,0 +1,167 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:marshaalexislikorawung_flutterwidget/mission.dart';
+
+class BookingPage extends StatefulWidget {
+  const BookingPage({super.key});
+
+  @override
+  State<BookingPage> createState() => _BookingPageState();
+}
+
+class _BookingPageState extends State<BookingPage> {
+  final _bookingKey = GlobalKey<FormState>();
+  final ctrlEmail = TextEditingController();
+  final ctrlFullname = TextEditingController();
+  final ctrlPhone = TextEditingController();
+  final ctrlCity = TextEditingController();
+
+  bool isHide = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Booking Page"),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(16),
+        child: Form(
+            key: _bookingKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: ctrlFullname,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    labelText: 'Fullname',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    final nameRegExp = RegExp(r'^[a-zA-Z ]+$');
+                    return !nameRegExp.hasMatch(value.toString())
+                        ? 'Please enter a valid name'
+                        : null;
+                  },
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: ctrlEmail,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email Address',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    return !EmailValidator.validate(value.toString())
+                        ? 'Email tidak valid!'
+                        : null;
+                  },
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: ctrlPhone,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.phone),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    final phoneRegExp = RegExp(r'^\+?0\d{9,}$');
+                    return !phoneRegExp.hasMatch(value.toString())
+                        ? 'Please enter a valid phone number'
+                        : null;
+                  },
+                ),
+                SizedBox(height: 24),
+                TextFormField(
+                  controller: ctrlCity,
+                  keyboardType: TextInputType.streetAddress,
+                  decoration: InputDecoration(
+                    labelText: 'City',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.location_city),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    final cityRegExp = RegExp(r'^[a-zA-Z ]+$');
+                    return !cityRegExp.hasMatch(value.toString())
+                        ? 'Please enter a valid city name'
+                        : null;
+                  },
+                ),
+                  const SizedBox(height: 24,),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    if (ctrlFullname.text.isEmpty ||
+                        ctrlEmail.text.isEmpty ||
+                        ctrlPhone.text.isEmpty ||
+                        ctrlCity.text.isEmpty) {
+                      // Show dialog if any field is empty
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Error'),
+                            content: Text('Please fill in all fields.'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: ()=> Navigator.pop(context, 'OK'),
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      // Show booking confirmation dialog
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Booking Confirmation"),
+                            content: Text(
+                                "Name: ${ctrlFullname.text} \n\n Email: ${ctrlEmail.text} \n\n Phone: ${ctrlPhone.text} \n\n City: ${ctrlCity.text}"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                     Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return MissionPage();
+                        }));
+                  },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  icon: Icon(Icons.save),
+                  label: Text("Book Now"),
+                  style: ElevatedButton.styleFrom(
+                      elevation: 2,
+                      padding: EdgeInsets.fromLTRB(24, 12, 24, 12),
+                      backgroundColor: Colors.blue[700],
+                      foregroundColor: Colors.white,
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                      )),
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
--- lib/mission.dart
+++ lib/mission.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:marshaalexislikorawung_flutterwidget/booking.dart';
 
 class MissionPage extends StatefulWidget {
   const MissionPage({super.key});
@@ -24,19 +26,60 @@ class _MissionPageState extends State<MissionPage> {
           title: const Text("Mission 1"),
           centerTitle: false,
         ), //appbar
-        floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-        floatingActionButton: Container(
-          margin: EdgeInsets.only(top: 90, right: 0), // Add margin to the FAB
-          child: FloatingActionButton(
-            onPressed: () {
-              toggleColor();
-            },
-            backgroundColor: Colors.white,
-            child: Icon(Icons.favorite, color: fabColor),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(28.0),
+        floatingActionButton: Stack(
+          children: [
+            Positioned(
+              top: 16.0, // Adjust as needed
+              right: 0.0, // Adjust as needed
+              child: Container(
+                margin:
+                    EdgeInsets.only(top: 160, right: 0), // Add margin to the FAB
+                child: FloatingActionButton(
+                  onPressed: () {
+                    toggleColor();
+                  },
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.favorite, color: fabColor),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(28.0),
+                    
+                  ),
+                  
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              top: 16.0, // Adjust as needed
+              right: 0.0, // Adjust as needed
+             child: Container(
+                margin:
+                    EdgeInsets.only(top: 770, right: 0), // Add margin to the FAB
+                    child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Colors.blue[200]!, Colors.blue[50]!],
+                  ),
+                   borderRadius: BorderRadius.circular(20.0)// Makes it circular
+                ),
+                child: FloatingActionButton.extended(
+                  onPressed: () {
+                     Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) {
+                          return BookingPage();
+                        }));
+                  },
+                  label: Text('Book Now'),
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  foregroundColor: Colors.black, // Remove elevation to match design
+                  
+                )
+              ),
+            ),
+            )
+          ],
         ),
         body: Container(
           width: double.infinity,
@@ -135,11 +178,11 @@ class _MissionPageState extends State<MissionPage> {
                         margin: EdgeInsets.all(8),
                         child: SingleChildScrollView(
                           child: Text(
-                              "A dedicated cat veterinarian plays a crucial role in ensuring the well-being of our feline companions. These specialized professionals undergo rigorous training to understand the unique physiology and behavior of cats. From routine check-ups to complex surgeries, they provide a wide range of services to keep our furry friends in optimal health. Their compassion and expertise make them invaluable members of any pet owner's support system. \n \nOne of the key responsibilities of a cat veterinarian is preventative care. They advise on vaccinations, nutrition, and exercise routines tailored to a cat's specific needs. Through regular examinations, they can catch and address potential health issues before they escalate. This proactive approach helps to extend the lifespan of our beloved pets and ensures they lead happy, fulfilling lives. \n \nIn addition to general care, cat veterinarians are adept at diagnosing and treating a variety of medical conditions. They utilize cutting-edge technology and their extensive knowledge of feline medicine to provide accurate assessments. Whether it's managing chronic illnesses, performing surgeries, or administering specialized treatments, they work tirelessly to alleviate any discomfort our cats may experience. \n \nBeyond their medical expertise, cat veterinarians are often a source of comfort and support for pet owners. They offer guidance on behavior training, offer advice on providing a stimulating environment, and are there to lend a sympathetic ear during difficult times. Their commitment to the well-being of cats and their dedication to fostering strong bonds between pets and their owners make them invaluable members of the veterinary community.",
-                              style: TextStyle(
-                                fontSize: 15.0, // Increased font size
-                              ),
-                              ),
+                            "A dedicated cat veterinarian plays a crucial role in ensuring the well-being of our feline companions. These specialized professionals undergo rigorous training to understand the unique physiology and behavior of cats. From routine check-ups to complex surgeries, they provide a wide range of services to keep our furry friends in optimal health. Their compassion and expertise make them invaluable members of any pet owner's support system. \n \nOne of the key responsibilities of a cat veterinarian is preventative care. They advise on vaccinations, nutrition, and exercise routines tailored to a cat's specific needs. Through regular examinations, they can catch and address potential health issues before they escalate. This proactive approach helps to extend the lifespan of our beloved pets and ensures they lead happy, fulfilling lives. \n \nIn addition to general care, cat veterinarians are adept at diagnosing and treating a variety of medical conditions. They utilize cutting-edge technology and their extensive knowledge of feline medicine to provide accurate assessments. Whether it's managing chronic illnesses, performing surgeries, or administering specialized treatments, they work tirelessly to alleviate any discomfort our cats may experience. \n \nBeyond their medical expertise, cat veterinarians are often a source of comfort and support for pet owners. They offer guidance on behavior training, offer advice on providing a stimulating environment, and are there to lend a sympathetic ear during difficult times. Their commitment to the well-being of cats and their dedication to fostering strong bonds between pets and their owners make them invaluable members of the veterinary community.",
+                            style: TextStyle(
+                              fontSize: 15.0, // Increased font size
+                            ),
+                          ),
                         ),
                         width: double.infinity,
                         height: double.infinity,
